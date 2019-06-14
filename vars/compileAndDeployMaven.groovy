78eff202279f4b36209556b64d25164b860c3a16
Justin <webmaster@cl6web.com>
2019-06-14 01:58:53 -0400

    feat: Jenkins!
    feat: "Hello World!" for pipeline

---
+++ vars/compileAndDeployMaven.groovy
@@ -0,0 +1,32 @@
+def call(body) {
+  pipelineParams = [:]
+  repo = pipelineParams.repo
+  version = pipelineParams.version
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+  pipeline {
+    agent {
+      docker {
+        image 'maven:3-alpine'
+        args '-v /root/.m2:/root/.m2'
+      }
+    }
+    options {
+      skipStagesAfterUnstable()
+    }
+    stages {
+      stage('build') {
+        steps {
+          sh 'mvn -B -DskipTests clean package'
+        }
+      }
+      stage('deliver') {
+        steps {
+          sh "mvn jar:jar install:install help:evaluate -Dexpression=project.name"
+        }
+      }
+    }
+  }
+}
