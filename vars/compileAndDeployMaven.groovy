8d5254d21f86656337e606e41ccd5a62014ae752
jarosenthal <jrosenthal96@gmail.com>
2019-06-24 21:17:54 -0400

    target folder path override

--- vars/compileAndDeployMaven.groovy
+++ vars/compileAndDeployMaven.groovy
@@ -4,6 +4,11 @@ def call(body) {
   body.delegate = pipelineParams
   body()
 
+  targetPath = ""
+  if (pipelineParams.target_path) {
+    targetPath = pipelineParams.target_path
+  }
+
   pipeline {
     agent {
       docker {
@@ -26,37 +31,47 @@ def call(body) {
       stage('build') {
         steps {
           sh 'mvn -DskipTests -U clean package'
+
+          dir(targetPath) {
+            sh "mvn help:effective-pom -Doutput=target/effective-pom.pom"
+          }
         }
       }
       stage('deploy release') {
         steps {
-          configFileProvider([configFile(fileId: 'artifactory-settings', variable: 'SETTINGS')]) {
-            sh "mvn deploy -s ${SETTINGS} -DskipTests -Dartifactory_url=https://artifactory.battleplugins.org/artifactory/"
+          script {
+            configFileProvider([configFile(fileId: 'artifactory-settings', variable: 'SETTINGS')]) {
+              sh "mvn deploy -s ${SETTINGS} -DskipTests -Dartifactory_url=https://artifactory.battleplugins.org/artifactory/"
+            }
+            dir(targetPath) {
+              pom = readMavenPom file: "target/effective-pom.pom"
+              archiveArtifacts artifacts: "target/${pom.build.finalName}.jar", excludes: "original-*.jar", fingerprint: true
+            }
           }
-          archiveArtifacts artifacts: 'target/*.jar', excludes: "target/original-*.jar", fingerprint: true
         }
       }
       stage('git release') {
         when { expression { BRANCH_NAME == 'master' } }
         steps {
           script {
-            sh "mvn help:effective-pom -Doutput=target/effective-pom.pom"
-            pom = readMavenPom file: 'target/effective-pom.pom'
-            withCredentials([string(credentialsId: 'github-token', variable: 'TOKEN')]) {
-              stdOut = sh returnStdout: true,
-                  script: "curl -H \"Content-type: application/json\" -H \"Authorization: token ${TOKEN}\" -d '{\n" +
-                      "\"tag_name\": \"${pom.version}\",\n" +
-                      "\"target_commitish\": \"master\",\n" +
-                      "\"name\": \"v${pom.version}\",\n" +
-                      "\"draft\": false,\n" +
-                      "\"prerelease\": false\n" +
-                      "}' https://api.github.com/repos/BattlePlugins/${pipelineParams.repo}/releases"
+            dir(targetPath){
+              pom = readMavenPom file: "target/effective-pom.pom"
+              withCredentials([string(credentialsId: 'github-token', variable: 'TOKEN')]) {
+                stdOut = sh returnStdout: true,
+                    script: "curl -H \"Content-type: application/json\" -H \"Authorization: token ${TOKEN}\" -d '{\n" +
+                        "\"tag_name\": \"${pom.version}\",\n" +
+                        "\"target_commitish\": \"master\",\n" +
+                        "\"name\": \"v${pom.version}\",\n" +
+                        "\"draft\": false,\n" +
+                        "\"prerelease\": false\n" +
+                        "}' https://api.github.com/repos/BattlePlugins/${pipelineParams.repo}/releases"
 
-              json = readJSON text: stdOut
-              if (json.upload_url) {
-                sh "curl -H \"Content-type: application/java-archive\" -H \"Authorization: token ${TOKEN}\" --upload-file target/${pom.build.finalName}.jar ${json.upload_url}=${pom.build.finalName}.jar"
-              } else {
-                echo "No upload_url found, is this a new release?"
+                json = readJSON text: stdOut
+                if (json.upload_url) {
+                  sh "curl -H \"Content-type: application/java-archive\" -H \"Authorization: token ${TOKEN}\" --upload-file target/${pom.build.finalName}.jar ${json.upload_url}=${pom.build.finalName}.jar"
+                } else {
+                  echo "No upload_url found, is this a new release?"
+                }
               }
             }
           }
